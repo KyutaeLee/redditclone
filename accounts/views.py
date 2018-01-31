94e78f839cadac17ccc0007435b90086feb35836
kyutaelee <rbfjqn@gmail.com>
2018-01-31 11:48:16 +0900

    app posts

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 
@@ -8,25 +8,25 @@ def signup(request):
         if request.POST['password1'] == request.POST['password2']:
             try:
                 user = User.objects.get(username=request.POST['username'])
-                return render(request, 'accounts/signup.html', {'error':"username has been already taken"})
+                return render(request, 'accounts/signup.html', {'error':'Username has alredy been taken'})
             except User.DoesNotExist:
-                User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
                 login(request, user)
                 return render(request, 'accounts/signup.html')
         else:
-            return render(request, 'accounts/signup.html', {'error':"passwords didn't match"})
+            return render(request, 'accounts/signup.html', {'error':'Passwords didn\'t match'})
     else:
-
         return render(request, 'accounts/signup.html')
 
 def loginview(request):
     if request.method == 'POST':
-        user = authenticate(username=request.POST['username'], password=request.POST['password1'])
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
         if user is not None:
             login(request, user)
-            return render(request, 'accounts/login.html', {'error':"Login successful!"})
+            if 'next' in request.POST:
+                return redirect(request.POST['next'])
+            return redirect('home')
         else:
-            return render(request, 'accounts/login.html', {'error':"The username and password didn't match"})
-
+            return render(request, 'accounts/login.html', {'error':'The Username and Password didn\'t match'})
     else:
         return render(request, 'accounts/login.html')
